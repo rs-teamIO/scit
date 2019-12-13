3de314a005cfec0f05392f85e81c3496ecc67c49
Katarina Tukelic <k.tukelic@5dnj19.com>
2019-12-13 18:14:55 +0100

    Dodate inicijalne verzije sema
    Napravljen kostur projekta (kontroleri, servisi, repozitorijumi), prvobitne verzije sema, kao i genericki validator za validaciju xml-a u odnosu na odgovarajucu semu

---
+++ backend/src/main/java/com/scit/xml/repository/PaperRepository.java
@@ -0,0 +1,4 @@
+package com.scit.xml.repository;
+
+public class PaperRepository {
+}
---
+++ backend/src/main/java/com/scit/xml/service/PaperService.java
@@ -0,0 +1,10 @@
+package com.scit.xml.service;
+
+import com.scit.xml.utility.XsdValidator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaperService {
+
+
+}
