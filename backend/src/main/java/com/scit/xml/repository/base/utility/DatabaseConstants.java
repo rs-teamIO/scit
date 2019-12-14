f5bb4507bfed892c72a074a844290162bc337508
Filip Mladenovic <filipeng97@gmail.com>
2019-12-14 12:28:47 +0100

    Changed database constants
    Ask assistant is that logic fine. Every document has own id and it is saved in collection (folder) where belongs

--- backend/src/main/java/com/scit/xml/repository/base/utility/DatabaseConstants.java
+++ backend/src/main/java/com/scit/xml/repository/base/utility/DatabaseConstants.java
@@ -1,8 +1,8 @@
-package com.scit.xml.repository.base.utils;
+package com.scit.xml.repository.base.utility;
 
 public class DatabaseConstants {
 
-	public static final String USERS_DOCUMENT_ID = "users";
+	//public static final String USERS_DOCUMENT_ID = "users";
 	public static final String USERS_COLLECTION_ID = "/db/scit/users";
 	
 }
