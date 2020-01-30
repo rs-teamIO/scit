3df244718344e6f5902d4d9f1552f76eea78ad78
Katarina Tukelic <tukelic.katarina@gmail.com>
2020-01-30 23:27:15 +0100

    Uploaded schema for review

--- backend/src/main/java/com/scit/xml/controller/ReviewController.java
+++ backend/src/main/java/com/scit/xml/controller/ReviewController.java
@@ -13,7 +13,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.mail.MessagingException;
 
